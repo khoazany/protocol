e1f456746ffdb33104513c5099a6f4c3fdb1f889
Brendan Chou <brendanchou@msn.com>
2018-09-17 17:09:25 -0700

    add BucketLenderWithRecoveryDelay

--- contracts/margin/external/payoutrecipients/DutchAuctionCloser.sol
+++ contracts/margin/external/payoutrecipients/DutchAuctionCloser.sol
@@ -179,6 +179,17 @@ contract DutchAuctionCloser is
             uint256 auctionEndTimestamp
         ) = getAuctionTimeLimits(positionId);
 
+        // prevent bidding before the auction
+        require(
+            block.timestamp >= auctionStartTimestamp,
+            "DutchAuctionCloser#getAuctionTimeLimits: Auction has not started"
+        );
+
+        // cost remains at zero after the auction has ended
+        if (block.timestamp > auctionEndTimestamp) {
+            return 0;
+        }
+
         // linearly decreases from maximum amount to zero over the course of the auction
         return MathHelpers.getPartialAmount(
             auctionEndTimestamp.sub(block.timestamp),
@@ -226,15 +237,6 @@ contract DutchAuctionCloser is
             auctionStartTimestamp = callTimestamp.add(callTimeLimit).sub(auctionLength);
             auctionEndTimestamp = callTimestamp.add(callTimeLimit);
         }
-
-        require(
-            block.timestamp >= auctionStartTimestamp,
-            "DutchAuctionCloser#getAuctionTimeLimits: Auction has not started"
-        );
-        require(
-            block.timestamp <= auctionEndTimestamp,
-            "DutchAuctionCloser#getAuctionTimeLimits: Auction has ended"
-        );
 
         return (
             auctionStartTimestamp,
